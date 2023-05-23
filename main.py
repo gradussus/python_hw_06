1ed30c10d8b1101083bd3f90ee932dd12befdf3b
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-05-23 07:56:49 +0300

    add transliteration

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import sys
+import re
 
 def transliteration (text):
     dict = {
+
         'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
         'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
         'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c',
@@ -13,3 +16,25 @@ def transliteration (text):
         'Ц': 'C', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E',
         'Ю': 'YU', 'Я': 'YA'
     }
+    newName = ''
+    for letter in text:
+        print (letter)
+        if re.match(r'[a-zA-Z0-9.]', letter):
+            newName += letter
+        elif letter in dict:
+            newName += dict[letter]
+        else:
+            newName += '_'
+    return newName
+
+def handle_files():
+    return
+
+def goClean():
+    if len(sys.argv) != 2:
+        return
+    else:
+        handle_files(sys.argv[1])
+
+goClean()
+
