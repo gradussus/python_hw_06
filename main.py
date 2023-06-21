ddc799f90faad10d76a9d0c63573f946c24c2559
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-06-21 15:36:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,57 @@ extensions = {
 unknown_extensions = set()
 known_extensions = set()
 
-def walk (path, init_path):
-    if os.listdir(path) == []:
-        os.rmdir(path)
-
+def goClean(args):
+    if len(args) != 2:
+        print('Enter the desired directory after the script name')
+        
+        return
     else:
+        initial_directory = args[1]
+        walk(initial_directory, initial_directory)
 
-        for item in os.listdir(path):
-            if item in extensions:
-                continue
-            
-            if os.path.isdir(os.path.join(path, item)):
-                walk(os.path.join(path, item), init_path)
+        print('{:^30}{}'.format('Unknown_extensions:', unknown_extensions))
+        print('{:^30}{}'.format('Known_extensions:', known_extensions))
 
-            else:
-                move_file(path, item, init_path)
+
+        if os.path.exists(os.path.join(initial_directory, 'images')):
+            print('{:^30}{}'.format('Images:', os.listdir(os.path.join(initial_directory, 'images') )))
+
+        if os.path.exists(os.path.join(initial_directory, 'videos')):
+            print('{:^30}{}'.format('Videos:', os.listdir(os.path.join(initial_directory, 'videos') )))
+
+        if os.path.exists(os.path.join(initial_directory, 'documents')):
+            print('{:^30}{}'.format('Documents:', os.listdir(os.path.join(initial_directory, 'documents') )))
+
+        if os.path.exists(os.path.join(initial_directory, 'music')):
+            print('{:^30}{}'.format('Music:', os.listdir(os.path.join(initial_directory, 'music') )))
+
+        if os.path.exists(os.path.join(initial_directory, 'archives')):
+            print('{:^30}{}'.format('Archives:', os.listdir(os.path.join(initial_directory, 'archives') )))
+
+def transliteration (text):
+
+    dict = {
+
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
+        'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+        'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c',
+        'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu',
+        'я': 'ya', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
+        'Ж': 'ZH', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
+        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H',
+        'Ц': 'C', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E',
+        'Ю': 'YU', 'Я': 'YA', 'Ї': 'YI', 'ї': 'yi', 'Є': 'YE', 'є': 'ye', 'І': 'I', 'і': 'i'
+    }
+    newName = ''
+    for letter in text:
+        if re.match(r'[a-zA-Z0-9.]', letter):
+            newName += letter
+        elif letter in dict:
+            newName += dict[letter]
+        else:
+            newName += '_'
+    return newName
 
 def move_file(old_folder, filename, init_path ):
  
@@ -90,61 +126,21 @@ def move_file(old_folder, filename, init_path ):
     else:
         unknown_extensions.add(filename.split('.')[-1])
 
+def walk (path, init_path):
+    if os.listdir(path) == []:
+        os.rmdir(path)
 
-# walk(r'C:\Users\shevc\Desktop\XXX')
-
-
-def transliteration (text):
-
-    dict = {
-
-        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
-        'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
-        'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c',
-        'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu',
-        'я': 'ya', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
-        'Ж': 'ZH', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
-        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H',
-        'Ц': 'C', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E',
-        'Ю': 'YU', 'Я': 'YA', 'Ї': 'YI', 'ї': 'yi', 'Є': 'YE', 'є': 'ye', 'І': 'I', 'і': 'i'
-    }
-    newName = ''
-    for letter in text:
-        if re.match(r'[a-zA-Z0-9.]', letter):
-            newName += letter
-        elif letter in dict:
-            newName += dict[letter]
-        else:
-            newName += '_'
-    return newName
-
-
-def goClean(args):
-    if len(args) != 2:
-        print('Enter the desired directory after the script name')
-        
-        return
     else:
-        initial_directory = args[1]
-        walk(initial_directory, initial_directory)
 
-        print('{:^30}{}'.format('Unknown_extensions:', unknown_extensions))
-        print('{:^30}{}'.format('Known_extensions:', known_extensions))
+        for item in os.listdir(path):
+            if item in extensions:
+                continue
+            
+            if os.path.isdir(os.path.join(path, item)):
+                walk(os.path.join(path, item), init_path)
 
+            else:
+                move_file(path, item, init_path)
 
-        if os.path.exists(os.path.join(initial_directory, 'images')):
-            print('{:^30}{}'.format('Images:', os.listdir(os.path.join(initial_directory, 'images') )))
-
-        if os.path.exists(os.path.join(initial_directory, 'videos')):
-            print('{:^30}{}'.format('Videos:', os.listdir(os.path.join(initial_directory, 'videos') )))
-
-        if os.path.exists(os.path.join(initial_directory, 'documents')):
-            print('{:^30}{}'.format('Documents:', os.listdir(os.path.join(initial_directory, 'documents') )))
-
-        if os.path.exists(os.path.join(initial_directory, 'music')):
-            print('{:^30}{}'.format('Music:', os.listdir(os.path.join(initial_directory, 'music') )))
-
-        if os.path.exists(os.path.join(initial_directory, 'archives')):
-            print('{:^30}{}'.format('Archives:', os.listdir(os.path.join(initial_directory, 'archives') )))
 
 goClean(sys.argv)
